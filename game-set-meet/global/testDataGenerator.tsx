f2fcc12893219af98207119ee985df327f12e793
Jordan Evans <jordanevans5588@gmail.com>
2023-06-13 16:38:53 -0400

    A bit more work bridging the API gap

--- game-set-meet/global/testDataGenerator.tsx
+++ game-set-meet/global/testDataGenerator.tsx
@@ -1,160 +1,228 @@
-// Function to generate a random string
-function randomString(length: number) {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
-
-// Function to generate a random number between min and max
-function randomNumber(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Function to generate a random boolean
-function randomBoolean() {
-  return Math.random() < 0.5;
-}
-
-const sports = ['Tennis', 'Badminton', 'Basketball'];
-
-let famousPlaces = [
+const courts: CourtData[] = [
   {
+    id: "Court1",
     name: "Great Pyramid of Giza",
-    latitude: 44.41611704341544,
-    longitude: -79.7036701416235
+    address: "Great Pyramid of Giza, Egypt",
+    lat: 29.9792,
+    lng: 31.1342
   },
   {
+    id: "Court2",
     name: "Statue of Liberty",
-    latitude: 40.6892,
-    longitude: -74.0445
+    address: "Liberty Island, New York, United States",
+    lat: 40.6892,
+    lng: -74.0445
   },
   {
+    id: "Court3",
     name: "Eiffel Tower",
-    latitude: 48.8584,
-    longitude: 2.2945
+    address: "Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France",
+    lat: 48.8584,
+    lng: 2.2945
   },
   {
+    id: "Court4",
     name: "Taj Mahal",
-    latitude: 27.1751,
-    longitude: 78.0421
+    address: "Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001, India",
+    lat: 27.1751,
+    lng: 78.0421
   },
   {
+    id: "Court5",
     name: "Sydney Opera House",
-    latitude: -33.8568,
-    longitude: 151.2153
+    address: "Bennelong Point, Sydney NSW 2000, Australia",
+    lat: -33.8568,
+    lng: 151.2153
   },
   {
+    id: "Court6",
     name: "Christ the Redeemer",
-    latitude: -22.9519,
-    longitude: -43.2105
+    address: "Parque Nacional da Tijuca - Alto da Boa Vista, Rio de Janeiro - RJ, Brazil",
+    lat: -22.9519,
+    lng: -43.2105
   },
   {
+    id: "Court7",
     name: "Great Wall of China",
-    latitude: 40.4319,
-    longitude: 116.5704
+    address: "Huairou District, China",
+    lat: 40.4319,
+    lng: 116.5704
   },
   {
+    id: "Court8",
     name: "Colosseum",
-    latitude: 41.8902,
-    longitude: 12.4922
+    address: "Piazza del Colosseo, 1, 00184 Roma RM, Italy",
+    lat: 41.8902,
+    lng: 12.4922
   },
   {
+    id: "Court9",
     name: "Machu Picchu",
-    latitude: -13.1631,
-    longitude: -72.5450
+    address: "08680, Peru",
+    lat: -13.1631,
+    lng: -72.5450
   },
   {
+    id: "Court10",
     name: "Petra",
-    latitude: 30.3285,
-    longitude: 35.4444
+    address: "Petra District, Jordan",
+    lat: 30.3285,
+    lng: 35.4444
   }
 ];
 
-
-// Array of first names and pronouns
-const firstNames = [
-  { name: 'John', pronouns: 'He/Him' },
-  { name: 'Jane', pronouns: 'She/Her' },
-  { name: 'Alex', pronouns: 'They/Them' },
-  // Add more as needed
+const sports: SportData[] = [
+  {
+    id: "Sport1",
+    name: "Tennis",
+    icon: "tennis-icon",
+    skillLevel: "Intermediate"
+  },
+  {
+    id: "Sport2",
+    name: "Soccer",
+    icon: "soccer-icon",
+    skillLevel: "Beginner"
+  },
+  {
+    id: "Sport3",
+    name: "Basketball",
+    icon: "basketball-icon",
+    skillLevel: "Advanced"
+  },
+  {
+    id: "Sport4",
+    name: "Badminton",
+    icon: "badminton-icon",
+    skillLevel: "Intermediate"
+  },
+  {
+    id: "Sport5",
+    name: "Golf",
+    icon: "golf-icon",
+    skillLevel: "Advanced"
+  },
+  {
+    id: "Sport6",
+    name: "Baseball",
+    icon: "baseball-icon",
+    skillLevel: "Beginner"
+  },
+  {
+    id: "Sport7",
+    name: "Volleyball",
+    icon: "volleyball-icon",
+    skillLevel: "Intermediate"
+  },
+  {
+    id: "Sport8",
+    name: "Cricket",
+    icon: "cricket-icon",
+    skillLevel: "Advanced"
+  },
+  {
+    id: "Sport9",
+    name: "Rugby",
+    icon: "rugby-icon",
+    skillLevel: "Beginner"
+  },
+  {
+    id: "Sport10",
+    name: "Table Tennis",
+    icon: "table-tennis-icon",
+    skillLevel: "Intermediate"
+  }
 ];
 
-// Array of last names
-const lastNames = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Miller', 'Davis'];
+// Generate Users Data
 
-// Array of common park names
-const parkNames = ['Central Park', 'Liberty Park', 'Lincoln Park', 'Riverside Park', 'Sunset Park'];
-
-// Function to generate random full names and corresponding pronouns
-function generateFullNameAndPronouns() {
-  const firstName = firstNames[randomNumber(0, firstNames.length - 1)];
-  const lastName = lastNames[randomNumber(0, lastNames.length - 1)];
-  return {
-    name: `${firstName.name} ${lastName}`,
-    pronouns: firstName.pronouns,
-  };
-}
-
-// Function to generate a random park name
-function generateParkName() {
-  return parkNames[randomNumber(0, parkNames.length - 1)];
-}
-
-// Function to generate a random small bio
-function generateBio() {
-  const bioTemplates = [
-    `I love playing ${sports[randomNumber(0, sports.length - 1)]}. Been playing for ${randomNumber(1, 20)} years.`,
-    `Looking for someone to play ${sports[randomNumber(0, sports.length - 1)]} with.`,
-    `I play ${sports[randomNumber(0, sports.length - 1)]} every ${['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][randomNumber(0, 6)]}.`,
+export function generateUsersData(): UserData[] {
+  return [
+    {
+      id: "User1",
+      name: "John Doe",
+      pronouns: "He/Him",
+      imgUrl: "https://i.pravatar.cc/300?img=1",
+      bio: "I love playing Tennis. Been playing for 5 years.",
+      sports: [
+        {
+          id: "Sport1",
+          name: "Tennis",
+          icon: "tennis-ball",
+          skillLevel: "3"
+        },
+        // You can add more sports here
+      ],
+      courts: [
+        {
+          id: "Court1",
+          name: "Great Pyramid of Giza",
+          address: "Great Pyramid of Giza, Egypt",
+          lat: 44.41611704341544,
+          lng: -79.7036701416235
+        },
+        // You can add more courts here
+      ],
+      availability: [
+        [true, false, true],
+        // Add more days availability here
+      ]
+    },
+    // Add more users here
   ];
-  return bioTemplates[randomNumber(0, bioTemplates.length - 1)];
 }
 
-// Generate the test user data
-function generateTestUserData() {
-  // Generate sports data
-  const userSports = sports.map(sport => ({
-    id: randomString(10),
-    name: sport,
-    icon: 'tennis-ball',
-    skillLevel: randomNumber(1, 5).toString(),
-  }));
+// Generate Connection Data
+export function generateConnectionData(): ConnectionData[] {
+  return [
+    {
+      id: "Connection1",
+      connectionId: "User1",
+      userId: "LoggedUser1",
+      lastMessage: {
+        id: "Message1",
+        senderId: "User1",
+        receiverId: "LoggedUser1",
+        content: "Hello there!",
+        timestamp: 1622462400000 // this corresponds to May 31, 2021
+      },
+    },
+    // Add more connections here
+  ];
+}
 
-  // Generate court data
-  const userCourts = [];
-  for (let i = 0; i < 3; i++) {
-    const randomPlace = famousPlaces[randomNumber(0, famousPlaces.length - 1)];
-    userCourts.push({
-      id: randomString(10),
-      name: randomPlace.name,
-      address: randomPlace.name,
-      lat: randomPlace.latitude,
-      lng: randomPlace.longitude,
-    });
-  }
-
-  const fullNameAndPronouns = generateFullNameAndPronouns();
-  const availability = new Array(7).fill(null).map(() => new Array(3).fill(null).map(() => randomBoolean()));
-
-  const availabilityStr = availability.map(day => day.map(hour => (hour ? '1' : '0')).join('')).join('\n');
-
-  const userData = {
-    id: randomString(10),
-    name: fullNameAndPronouns.name,
-    pronouns: fullNameAndPronouns.pronouns,
-    imgUrl: `https://i.pravatar.cc/300`,
-    bio: generateBio(),
-    sports: userSports,
-    courts: userCourts,
-    availability,
-    availabilityStr,
-    connections: randomNumber(0, 100),
+// Generate Logged User Data
+export function generateLoggedUserData(): LoggedUserData {
+  return {
+    id: "LoggedUser1",
+    name: "Jane Doe",
+    pronouns: "She/Her",
+    imgUrl: "https://i.pravatar.cc/300?img=2",
+    bio: "I love playing Badminton. Been playing for 7 years.",
+    sports: [
+      {
+        id: "Sport2",
+        name: "Badminton",
+        icon: "badminton-ball",
+        skillLevel: "4"
+      },
+      // You can add more sports here
+    ],
+    courts: [
+      {
+        id: "Court2",
+        name: "Statue of Liberty",
+        address: "Statue of Liberty, USA",
+        lat: 40.6892,
+        lng: -74.0445
+      },
+      // You can add more courts here
+    ],
+    availability: [
+      [false, true, false],
+      // Add more days availability here
+    ],
+    email: "jane.doe@example.com",
+    connections: generateConnectionData(),
   };
-
-  return userData;
 }
-
-export default generateTestUserData;
