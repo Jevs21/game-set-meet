727b706b10e1a04ab2358806ead46f2e398b5422
Jordan Evans <jordanevans5588@gmail.com>
2023-07-01 08:03:28 -0400

    cottage. Thats it for today

--- game-set-meet/components/ProfileCard/ProfileCardMutualDetails.tsx
+++ game-set-meet/components/ProfileCard/ProfileCardMutualDetails.tsx
@@ -1,7 +1,10 @@
+import { View } from "react-native";
 
 const ProfileCardMutualDetails = () => {
   return (
-    <></>
+    <View style={{flex: 1}}>
+      TODO: Parse mutual user details 
+    </View>
   );
 }
 
